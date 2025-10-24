b774ffbb6bbcf463123dec6ec3d42ccea5d0e46d
zhensherlock <zhensherlock@126.com>
2025-10-24 11:13:25 +0800

    chore: update dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,14 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    projects: [
-      'packages/*/vitest.config.ts',
-    ],
+    projects: ['packages/*/vitest.config.ts'],
     coverage: {
       provider: 'v8',
-      include: [
-        'packages/*/src/*.ts',
-      ],
+      include: ['packages/*/src/*.ts'],
       reporter: ['json'],
       reportsDirectory: './coverage/tmp',
     },
