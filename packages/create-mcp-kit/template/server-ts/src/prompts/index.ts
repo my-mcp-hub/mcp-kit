680715b444aeb46f856ddec1212aec773ebadcdb
zhensherlock <zhensherlock@126.com>
2025-11-28 09:33:25 +0800

    feat: update server-ts template

--- packages/create-mcp-kit/template/server-ts/src/prompts/index.ts
+++ packages/create-mcp-kit/template/server-ts/src/prompts/index.ts
@@ -1,5 +1,5 @@
-import { z } from 'zod'
 import type { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
+import { z } from 'zod'
 
 export const registerPrompts = (server: McpServer) => {
   server.registerPrompt(
